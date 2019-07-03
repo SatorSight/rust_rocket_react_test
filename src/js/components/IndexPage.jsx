9439ca54e6e50c83ad4c9032807c54ca6004788a
Eugene Nikolaev <eugene.nikolaev@globalsign.com>
2019-07-03 16:59:03 +0900

    css + load data from backend

--- src/js/components/IndexPage.jsx
+++ src/js/components/IndexPage.jsx
@@ -5,12 +5,38 @@ import 'whatwg-fetch';
 class IndexPage extends React.Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            users: [],
+            stagings: [],
+        };
+    }
+
+    componentDidMount(){
+        fetch('/')
+            //todo set content-type on backend
+            .then(r => r.json())
+            .then(r => {
+                this.setState({
+                    users: r.users,
+                    stagings: r.stagings
+                });
+            })
     }
 
     render() {
         return (
             <div className="container">
-                dddddfgdgjg
+                <div className="users">
+                    {this.state.users.map(user =>
+                        <div key={user.id}>{user.name}</div>
+                    )}
+                </div>
+                <div className="stagings">
+                    {this.state.stagings.map(staging =>
+                        <div key={staging.id}>{staging.name}</div>
+                    )}
+                </div>
             </div>
         );
     }
